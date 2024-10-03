e3fc10068a0626875825863bb55f454520705251
Negix <info@jakubhyza.cz>
2024-10-03 19:06:07 +0200

    Ready to deploy beta

--- client/src/pages/ingame/sidebar/RoomOverview.svelte
+++ client/src/pages/ingame/sidebar/RoomOverview.svelte
@@ -2,17 +2,34 @@
 	import { IngameRoom } from "../../../lib/networking/room";
 
 </script>
-<div>
+<!-- <div>
 	<div>
 		Stat
 	</div>
 	<div>
-		<button class="button button--leade">
-			<img src="/img/icons/leave.png" alt="Odhlásit se" class="icon invert" draggable="false" />
-		</button>
+
 	</div>
 </div>
 
 <div>
 	{$IngameRoom?.name ?? 'Načítání...'}
+</div> -->
+
+<div class="wrapper">
+	<button class="button button--leave">
+		<img src="/img/icons/leave.png" alt="Odhlásit se" class="icon invert" draggable="false" />
+		Opustit místnost
+	</button>
 </div>
+
+<style>
+	.wrapper {
+		padding: 1rem;
+		text-align: center;
+		width: 100%;
+	}
+	.wrapper .button {
+		margin-left: auto;
+		margin-right: auto;
+	}
+</style>
