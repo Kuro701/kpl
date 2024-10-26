21b09e609b59b1e17623e08f6d9ee6e4badbbe79
Negix <info@jakubhyza.cz>
2024-10-26 23:10:03 +0200

    Bugfix

--- client/src/components/cards/Card.svelte
+++ client/src/components/cards/Card.svelte
@@ -26,7 +26,7 @@
       {/if}
 
       <slot name="front">
-        <p>{text.replaceAll(/_+/, '______')}</p>
+        <p>{text.replaceAll(/_+/g, '______')}</p>
         <img
           src={`/img/logo${black ? "_white" : ""}.png`}
           alt="Karty proti lidskosti"
