68a14ffa2cf0cd574b2e42d0034d894354c5f858
Sára Hýžová <sarah@sarahyzova.cz>
2026-02-01 09:47:26 +0100

    Remove Helkor.eu link from layout menu footer

--- client/src/components/layout/LayoutMenu.svelte
+++ client/src/components/layout/LayoutMenu.svelte
@@ -15,7 +15,6 @@
 		<slot />
 	</div>
 	<div class="layout-menu__footer">
-		<div>Hosted by <a href="https://link.helkor.eu/from-kpl" target="_blank">Helkor.eu</a></div>
 		<div>By <a href="https://sarahyzova.cz/" target="_blank">Sára Hýžová</a> with ❤️ and a lot of ☕</div>
 		<div>2018 - {yearNow}</div>
 	</div>
